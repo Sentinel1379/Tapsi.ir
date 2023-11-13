4a07145be9da4c8b421738e9ef44c764a66c0042
Sentinel1379 <sadeghanalooee31@gmail.com>
2023-11-13 10:40:26 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,214 @@
+function classicFun() {
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    var img3 = document.getElementById("img3");
+    var img4 = document.getElementById("img4");
+    var img5 = document.getElementById("img5");
+    var img6 = document.getElementById("img6");
+    var img7 = document.getElementById("img7");
+    var dis1 = document.getElementsByClassName("services-discribtion");
+
+    img1.style.filter = 'opacity(1)';
+    img2.style.filter = 'opacity(.35)';
+    img3.style.filter = 'opacity(.35)';
+    img4.style.filter = 'opacity(.35)';
+    img5.style.filter = 'opacity(.35)';
+    img6.style.filter = 'opacity(.35)';
+    img7.style.filter = 'opacity(.35)';
+    dis1[0].style.display = 'flex';
+    dis1[1].style.display = 'none';
+    dis1[2].style.display = 'none';
+    dis1[3].style.display = 'none';
+    dis1[4].style.display = 'none';
+    dis1[5].style.display = 'none';
+    dis1[6].style.display = 'none';
+}
+
+function telFun() {
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    var img3 = document.getElementById("img3");
+    var img4 = document.getElementById("img4");
+    var img5 = document.getElementById("img5");
+    var img6 = document.getElementById("img6");
+    var img7 = document.getElementById("img7");
+    var dis1 = document.getElementsByClassName("services-discribtion");
+
+    img1.style.filter = 'opacity(.35)';
+    img2.style.filter = 'opacity(1)';
+    img3.style.filter = 'opacity(.35)';
+    img4.style.filter = 'opacity(.35)';
+    img5.style.filter = 'opacity(.35)';
+    img6.style.filter = 'opacity(.35)';
+    img7.style.filter = 'opacity(.35)';
+    dis1[0].style.display = 'none';
+    dis1[1].style.display = 'flex';
+    dis1[2].style.display = 'none';
+    dis1[3].style.display = 'none';
+    dis1[4].style.display = 'none';
+    dis1[5].style.display = 'none';
+    dis1[6].style.display = 'none';
+}
+
+function lineFun() {
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    var img3 = document.getElementById("img3");
+    var img4 = document.getElementById("img4");
+    var img5 = document.getElementById("img5");
+    var img6 = document.getElementById("img6");
+    var img7 = document.getElementById("img7");
+    var dis1 = document.getElementsByClassName("services-discribtion");
+
+    img1.style.filter = 'opacity(.35)';
+    img2.style.filter = 'opacity(.35)';
+    img3.style.filter = 'opacity(1)';
+    img4.style.filter = 'opacity(.35)';
+    img5.style.filter = 'opacity(.35)';
+    img6.style.filter = 'opacity(.35)';
+    img7.style.filter = 'opacity(.35)';
+    dis1[0].style.display = 'none';
+    dis1[1].style.display = 'none';
+    dis1[2].style.display = 'flex';
+    dis1[3].style.display = 'none';
+    dis1[4].style.display = 'none';
+    dis1[5].style.display = 'none';
+    dis1[6].style.display = 'none';
+}
+
+function motoFun() {
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    var img3 = document.getElementById("img3");
+    var img4 = document.getElementById("img4");
+    var img5 = document.getElementById("img5");
+    var img6 = document.getElementById("img6");
+    var img7 = document.getElementById("img7");
+    var dis1 = document.getElementsByClassName("services-discribtion");
+
+    img1.style.filter = 'opacity(.35)';
+    img2.style.filter = 'opacity(.35)';
+    img3.style.filter = 'opacity(.35)';
+    img4.style.filter = 'opacity(1)';
+    img5.style.filter = 'opacity(.35)';
+    img6.style.filter = 'opacity(.35)';
+    img7.style.filter = 'opacity(.35)';
+    dis1[0].style.display = 'none';
+    dis1[1].style.display = 'none';
+    dis1[2].style.display = 'none';
+    dis1[3].style.display = 'flex';
+    dis1[4].style.display = 'none';
+    dis1[5].style.display = 'none';
+    dis1[6].style.display = 'none';
+}
+
+function autoFun() {
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    var img3 = document.getElementById("img3");
+    var img4 = document.getElementById("img4");
+    var img5 = document.getElementById("img5");
+    var img6 = document.getElementById("img6");
+    var img7 = document.getElementById("img7");
+    var dis1 = document.getElementsByClassName("services-discribtion");
+
+    img1.style.filter = 'opacity(.35)';
+    img2.style.filter = 'opacity(.35)';
+    img3.style.filter = 'opacity(.35)';
+    img4.style.filter = 'opacity(.35)';
+    img5.style.filter = 'opacity(1)';
+    img6.style.filter = 'opacity(.35)';
+    img7.style.filter = 'opacity(.35)';
+    dis1[0].style.display = 'none';
+    dis1[1].style.display = 'none';
+    dis1[2].style.display = 'none';
+    dis1[3].style.display = 'none';
+    dis1[4].style.display = 'flex';
+    dis1[5].style.display = 'none';
+    dis1[6].style.display = 'none';
+}
+
+function plusFun() {
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    var img3 = document.getElementById("img3");
+    var img4 = document.getElementById("img4");
+    var img5 = document.getElementById("img5");
+    var img6 = document.getElementById("img6");
+    var img7 = document.getElementById("img7");
+    var dis1 = document.getElementsByClassName("services-discribtion");
+
+    img1.style.filter = 'opacity(.35)';
+    img2.style.filter = 'opacity(.35)';
+    img3.style.filter = 'opacity(.35)';
+    img4.style.filter = 'opacity(.35)';
+    img5.style.filter = 'opacity(.35)';
+    img6.style.filter = 'opacity(1)';
+    img7.style.filter = 'opacity(.35)';
+    dis1[0].style.display = 'none';
+    dis1[1].style.display = 'none';
+    dis1[2].style.display = 'none';
+    dis1[3].style.display = 'none';
+    dis1[4].style.display = 'none';
+    dis1[5].style.display = 'flex';
+    dis1[6].style.display = 'none';
+}
+
+function hamyarFun() {
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    var img3 = document.getElementById("img3");
+    var img4 = document.getElementById("img4");
+    var img5 = document.getElementById("img5");
+    var img6 = document.getElementById("img6");
+    var img7 = document.getElementById("img7");
+    var dis1 = document.getElementsByClassName("services-discribtion");
+
+    img1.style.filter = 'opacity(.35)';
+    img2.style.filter = 'opacity(.35)';
+    img3.style.filter = 'opacity(.35)';
+    img4.style.filter = 'opacity(.35)';
+    img5.style.filter = 'opacity(.35)';
+    img6.style.filter = 'opacity(.35)';
+    img7.style.filter = 'opacity(1)';
+    dis1[0].style.display = 'none';
+    dis1[1].style.display = 'none';
+    dis1[2].style.display = 'none';
+    dis1[3].style.display = 'none';
+    dis1[4].style.display = 'none';
+    dis1[5].style.display = 'none';
+    dis1[6].style.display = 'flex';
+}
+
+var x1 ;
+var x2 ; 
+var x3 ;
+var x4 ;
+var x5 ;
+var x6 ;
+
+function showNav() {
+    classicFun();
+    x1 = setTimeout(telFun, 7000);
+    x2 = setTimeout(lineFun, 14000);
+    x3 = setTimeout(motoFun, 21000);
+    x4 = setTimeout(autoFun, 28000);
+    x5 = setTimeout(plusFun, 35000);
+    x6 = setTimeout(hamyarFun, 42000);
+}
+
+function offshow() {
+    clearInterval(id1);
+    clearTimeout(x1);
+    clearTimeout(x2);
+    clearTimeout(x3);
+    clearTimeout(x4);
+    clearTimeout(x5);
+    clearTimeout(x6);
+
+}
+
+
+
+showNav();
+const id1 = setInterval(showNav, 49000);
